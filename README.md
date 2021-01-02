e45fdb83f8fd665633b2a4fc36728ceee53c7596
cjun714 <cjun714@gmail.com>
2021-01-03 00:38:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+dl-httpfiles
+============
+
+Tool to scape file urls from http file sever*.
+
+
+## Install
+``` shell
+> go get -u github.com/cjun714/dl-httpfiles
+```
+
+## How to Use
+``` shell
+> dl-httpfiles "http://example.com/?path=./Comics/MARVEL/Comics/"
+```
+
---
+++ src/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	url := os.Args[1]
+	site := getSiteName(url)
+
+	get(site, url, "/")
+}
+
+func get(site, url, dirName string) error {
+	fmt.Println("Dir:", dirName)
+
+	// if not a full path, then add site to url
+	if strings.Index(url, "http://") == -1 {
+		url = site + url
+	}
+
+	resp, e := http.Get(url)
+	if e != nil {
+		return nil
+	}
+	defer resp.Body.Close()
+
+	doc, e := goquery.NewDocumentFromReader(resp.Body)
+	doc.Find("li").Each(func(i int, s *goquery.Selection) {
+		l, _ := s.Find("a").Attr("href")
+		fName := s.Text()
+		typ, exist := s.Attr("type")
+
+		if exist && typ == "circle" && fName == "@eaDir" {
+			return // skip '@eaDir'
+		}
+
+		if exist && typ == "circle" {
+			subDirName := dirName + fName + "/"
+			get(site, l, subDirName)
+		} else {
+			fmt.Println("File:", fName)
+			fmt.Println(site + l)
+		}
+	})
+
+	fmt.Println("-----------------------------------------")
+
+	return nil
+}
+
+func getSiteName(url string) string {
+	tmpURL := url[7:]
+	idx := strings.Index(tmpURL, "/")
+	return "http://" + tmpURL[:idx] + "/"
+}
